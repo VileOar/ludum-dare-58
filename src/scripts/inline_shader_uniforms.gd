31fa81f2bcb44e8153ac8087113f6e45060248de
Daniel Francisco <deny.jo@hotmail.com>
2025-10-05 00:09:19 +0100

    Change shader to tint the characters instead of calculating outlines

--- src/scripts/inline_shader_uniforms.gd
+++ src/scripts/inline_shader_uniforms.gd
@@ -1,10 +1,20 @@
 extends Node2D
 
-var mat
+var materials
 
 func _ready():
-	mat = load("res://assets/shaders/InlineMaterial.tres")
-	mat.set("shader_parameter/screen_resolution", DisplayServer.screen_get_size())
+	materials = [
+		load("res://assets/shaders/red_material.tres"),
+		load("res://assets/shaders/orange_material.tres"),
+		load("res://assets/shaders/yellow_material.tres"),
+		load("res://assets/shaders/green_material.tres"),
+		load("res://assets/shaders/blue_material.tres"),
+		load("res://assets/shaders/purple_material.tres")
+	]
+	
+	for mat in materials:
+		mat.set("shader_parameter/screen_resolution", DisplayServer.screen_get_size())
 
 func _process(_delta):
-	mat.set("shader_parameter/mouse_position", DisplayServer.mouse_get_position())
+	for mat in materials:
+		mat.set("shader_parameter/mouse_position", DisplayServer.mouse_get_position())
