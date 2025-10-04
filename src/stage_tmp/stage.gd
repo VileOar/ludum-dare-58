456b74852ebf76c8eb24b57942ab3bcfea9f6d03
Franciscorp <franciscorp1994@hotmail.com>
2025-10-04 17:15:35 +0100

    Added Tilemap, camera Movement, stage, bounds
    [Stage TMP] added a stage with a tilemap, bounds with collisions and a camera for the player to move around
    [Camera Controller] Added a camera controller with a script for basic camera movements inside the space of the tilemap wasd or arrows
    [Project settings] Added move_right, left, up, down
    [Tilemap] Added tilemap with temporary tileset from kennys to create and draw a map

---
+++ src/stage_tmp/stage.gd
@@ -0,0 +1,13 @@
+extends Node2D
+class_name StageScene
+
+#@onready var cam_anchor: Node2D = %CameraAnchor
+
+
+func _ready():
+	pass
+
+
+func set_cam_position(_pos: Vector2):
+	#cam_anchor.position = _pos
+	pass
