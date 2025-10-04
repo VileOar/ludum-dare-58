21091f4e4c0564d5259865b1b010093759ac7abf
Daniel Francisco <deny.jo@hotmail.com>
2025-10-05 00:34:19 +0100

    Add global variables to control shader uniforms of all materials

--- src/autoload/Global.gd
+++ src/autoload/Global.gd
@@ -4,6 +4,10 @@ extends Node
 # basic unit of measure
 const BLOCK := 64.0
 
+# mouse circle shader uniforms
+const MOUSE_CIRCLE_RADIUS := 150.0
+const MOUSE_CIRCLE_SMOOTH_WIDTH := 50.0
+
 enum Shapes {
 	POINTY,
 	BLOCKY,
--- src/scripts/mouse_circle_uniforms.gd
+++ src/scripts/mouse_circle_uniforms.gd
@@ -14,6 +14,8 @@ func _ready():
 	
 	for mat in materials:
 		mat.set("shader_parameter/screen_resolution", DisplayServer.screen_get_size())
+		mat.set("shader_parameter/circle_radius", Global.MOUSE_CIRCLE_RADIUS)
+		mat.set("shader_parameter/circle_smooth_width", Global.MOUSE_CIRCLE_SMOOTH_WIDTH)
 
 func _process(_delta):
 	for mat in materials:
