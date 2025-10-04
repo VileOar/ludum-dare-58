d249e0abe7ba3715cb47dfbccd71078a7ec3fc2f
Miguel Monteiro <66484852+exodusgd@users.noreply.github.com>
2025-10-04 19:32:06 +0100

    Score screen setup
    - Tweaked theme font color to an off-white color
    - Added score value globals
    - Created score screen scene and script (score calculation wip)

--- src/autoload/Global.gd
+++ src/autoload/Global.gd
@@ -3,3 +3,9 @@ extends Node
 
 # basic unit of measure
 const BLOCK := 64.0
+
+# base value of a collected soul
+const BASE_SCORE : int = 10
+# extra score awarded on soul rarity
+const RARE_BONUS: int = 10
+const LEGENDARY_BONUS: int = 20
