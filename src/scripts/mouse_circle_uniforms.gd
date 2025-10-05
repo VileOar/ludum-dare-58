d2ad44ddb94ed010e87bc5f0808bec14752be059
Daniel Francisco <deny.jo@hotmail.com>
2025-10-05 16:19:55 +0100

    Fix mouse position parameter for the shader

--- src/scripts/mouse_circle_uniforms.gd
+++ src/scripts/mouse_circle_uniforms.gd
@@ -3,11 +3,11 @@ extends Node2D
 
 func _ready():
 	for mat in Global.get_all_materials():
-		mat.set("shader_parameter/screen_resolution", DisplayServer.screen_get_size())
+		mat.set("shader_parameter/screen_resolution", get_viewport().get_visible_rect().size)
 		mat.set("shader_parameter/circle_radius", Global.MOUSE_CIRCLE_RADIUS)
 		mat.set("shader_parameter/circle_smooth_width", Global.MOUSE_CIRCLE_SMOOTH_WIDTH)
 
 
 func _process(_delta):
 	for mat in Global.get_all_materials():
-		mat.set("shader_parameter/mouse_position", DisplayServer.mouse_get_position())
+		mat.set("shader_parameter/mouse_position", get_viewport().get_mouse_position())
