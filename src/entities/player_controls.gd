8c41aac119de883b3abf78699d7b3d0ccea4889a
Daniel Francisco <deny.jo@hotmail.com>
2025-10-05 20:14:22 +0100

    Change slash so that it need to enter and exit collision to kill

--- src/entities/player_controls.gd
+++ src/entities/player_controls.gd
@@ -2,18 +2,25 @@ extends Node2D
 
 
 var is_clicked: bool = false
+var has_entered: Dictionary[Node2D, bool]
 
 
 func _input(event):
 	if event is InputEventMouseButton:
 		is_clicked = !is_clicked
 		$SlashTrail.is_active = is_clicked
+		if not is_clicked:
+			has_entered.clear()
 	
 	if event is InputEventMouseMotion:
 		$SlashArea.position = event.position
 
 
-# killing on enter, maybe kill only when enter and exit while holding mouse btn?
 func _on_slash_area_body_entered(body: Node2D) -> void:
 	if is_clicked:
+		has_entered[body] = true
+
+
+func _on_slash_area_body_exited(body: Node2D) -> void:
+	if is_clicked and has_entered.has(body) and has_entered[body]:
 		(body as Mook).queue_free()
