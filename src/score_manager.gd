6908a0ad46c3448dd37ea99767f52815f2e3dafc
Miguel Monteiro <66484852+exodusgd@users.noreply.github.com>
2025-10-05 12:27:44 +0100

    Code clean up
    - Cleaned up code for better readability

--- src/score_manager.gd
+++ src/score_manager.gd
@@ -1,21 +1,20 @@
 extends Node2D
 
+# store streak counts
 var _same_colour_streak: int = 1
 var _same_shape_streak: int = 1
-
 var _unique_shape_same_colour_streak: int = 1
 var _same_shape_unique_colour_streak: int = 1
-
+# count how many times each combo was made
 var _3_shapes_of_colour_counter: int = 0
 var _all_shapes_of_colour_counter: int = 0
 var _all_colours_of_shape_counter: int = 0
-
+# store how many unique shapes/colours exist and which ones are left to make a combo
 var _unique_shapes: Array
 var _unique_shapes_remaining: Array
-
 var _unique_colours: Array
 var _unique_colours_remaining: Array
-
+# store the stats of the last and second to last collected mook
 var _last_collected_mook: MookStats
 var _second_to_last_collected_mook: MookStats
 
@@ -36,46 +35,47 @@ func _reset_unique_colours_remaining() -> void:
 func _on_collect(collected_mook : Mook) -> void:
 	_second_to_last_collected_mook = _last_collected_mook
 	_last_collected_mook = collected_mook.get_stats()
-	
-	if(!_second_to_last_collected_mook):
+	# only check for combos if more than 1 mook has been collected
+	if !_second_to_last_collected_mook:
 		return
-	# the updates and checks below have to be done in this order
-	_update_same_colour_streak()
-	_update_unique_shape_same_colour_streak()
+	# first update the streak variables
+	_update_same_colour_streaks()
+	_update_same_shape_streaks()
+	# then check if any of the combos apply
 	_3_shapes_of_colour_check()
 	_all_shapes_of_colours_check()
-	
-	_update_same_shape_streak()
-	_update_same_shape_unique_colour_streak()
 	_all_colours_of_shape_check()
 
-func _update_same_colour_streak() -> void:
-	if(_last_collected_mook.colour == _second_to_last_collected_mook.colour):
+func _update_same_colour_streaks() -> void:
+	if _last_collected_mook.colour == _second_to_last_collected_mook.colour:
 		# continue the streak
 		_same_colour_streak += 1
 	else:
 		# reset the streak
 		_same_colour_streak = 1
+	_update_unique_shape_same_colour_streak()
 
-func _update_same_shape_streak() -> void:
-	if(_last_collected_mook.shape == _second_to_last_collected_mook.shape):
+func _update_same_shape_streaks() -> void:
+	if _last_collected_mook.shape == _second_to_last_collected_mook.shape:
 		# continue the streak
 		_same_shape_streak += 1
 	else:
 		# reset the streak
 		_same_shape_streak = 1
+	_update_same_shape_unique_colour_streak()
 
 func _update_unique_shape_same_colour_streak() -> void:
-	if(_same_colour_streak == 1):
+	if _same_colour_streak == 1:
 		_reset_unique_shape_same_colour_streak()
 		return
-	if(_last_collected_mook.shape == _second_to_last_collected_mook.shape):
+	if _last_collected_mook.shape == _second_to_last_collected_mook.shape:
 		_reset_unique_shape_same_colour_streak()
 		return
-	if(_unique_shapes_remaining.has(_last_collected_mook.shape)):
+	if _unique_shapes_remaining.has(_last_collected_mook.shape):
 		_unique_shapes_remaining.erase(_last_collected_mook.shape)
-		_unique_shapes_remaining.erase(_second_to_last_collected_mook.shape)
 		_unique_shape_same_colour_streak += 1
+		if _unique_shape_same_colour_streak == 2:
+			_unique_shapes_remaining.erase(_second_to_last_collected_mook.shape)
 	else:
 		_reset_unique_shape_same_colour_streak()
 
@@ -84,19 +84,20 @@ func _reset_unique_shape_same_colour_streak() -> void:
 	_reset_unique_shapes_remaining()
 
 func _update_same_shape_unique_colour_streak() -> void:
-	if(_same_shape_streak == 1):
+	if _same_shape_streak == 1:
 		_reset_same_shape_unique_colour_streak()
 		return
-	if(_last_collected_mook.colour == _second_to_last_collected_mook.colour):
+	if _last_collected_mook.colour == _second_to_last_collected_mook.colour:
 		_reset_same_shape_unique_colour_streak()
 		return
-	if(_unique_colours_remaining.is_empty()):
+	if _unique_colours_remaining.is_empty():
 		_reset_same_shape_unique_colour_streak()
 		return
-	if(	_unique_colours_remaining.has(_last_collected_mook.colour)):
+	if _unique_colours_remaining.has(_last_collected_mook.colour):
 		_unique_colours_remaining.erase(_last_collected_mook.colour)
-		_unique_colours_remaining.erase(_second_to_last_collected_mook.colour)
 		_same_shape_unique_colour_streak += 1
+		if _same_shape_unique_colour_streak == 2:
+			_unique_colours_remaining.erase(_second_to_last_collected_mook.colour)
 	else:
 		_reset_same_shape_unique_colour_streak()
 
@@ -106,16 +107,16 @@ func _reset_same_shape_unique_colour_streak() -> void:
 
 # checks if last 3 collected mooks are of the same colour and different shapes
 func _3_shapes_of_colour_check() -> void:
-	if(_unique_shape_same_colour_streak == 3):
+	if _unique_shape_same_colour_streak == 3:
 		_3_shapes_of_colour_counter += 1
 
 # checks if last 4 collected mooks are of the same colour and different shapes
 func _all_shapes_of_colours_check() -> void:
-	if(_unique_shape_same_colour_streak == _unique_shapes.size()):
+	if _unique_shape_same_colour_streak == _unique_shapes.size():
 		_all_shapes_of_colour_counter += 1
 		_3_shapes_of_colour_counter -= 1
 
 # checks if last 6 collected mooks are of the same shape and different colours
 func _all_colours_of_shape_check() -> void:
-	if(_same_shape_unique_colour_streak == _unique_colours.size()):
+	if _same_shape_unique_colour_streak == _unique_colours.size():
 		_all_colours_of_shape_counter += 1
