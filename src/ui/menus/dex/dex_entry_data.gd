66ba1a6060107166608cf7edaaf61dd0b3438dee
luischavesdev <115810776+luischavesdev@users.noreply.github.com>
2025-10-05 23:06:48 +0100

    sorting functions for dex

--- src/ui/menus/dex/dex_entry_data.gd
+++ src/ui/menus/dex/dex_entry_data.gd
@@ -1,15 +1,17 @@
 extends RefCounted
 class_name DexEntryData
 
-var shape : Global.Shapes
-var colour : Global.Colours
-var rarity : Global.Rarities
-var entry_name : String
+var entry_number := 0
+var shape: Global.Shapes
+var colour: Global.Colours
+var rarity: Global.Rarities
+var entry_name: String
 var acquired := false
 
 
-func init_vars(in_shape, in_colour, in_rarity, in_name):
+func init_vars(in_shape, in_colour, in_rarity, in_name, in_number):
 	shape = in_shape
 	colour = in_colour
 	rarity = in_rarity
 	entry_name = in_name
+	entry_number = in_number
--- src/ui/menus/dex/dex_menu.gd
+++ src/ui/menus/dex/dex_menu.gd
@@ -1,8 +1,26 @@
-extends ScrollContainer
+extends PanelContainer
 
 @export var entry_scene: PackedScene
 
+@onready var show_button: MenuButton = %ShowButton
+@onready var acquired_label: Label = %AcquiredNumber
+@onready var roaming_label: Label = %RoamingNumber
 @onready var list: VBoxContainer = %List
+
+
+func _ready():
+	acquired_label.text = str(DexManager.acquired_souls)
+	roaming_label.text = str(DexManager.roaming_souls)
+
+	#TODO: Show flags not yet implemented
+	#show_button.get_popup().id_pressed.connect(_hello_there)
+
+	populate_list()
+
+
+func refresh():
+	#TODO: possibly needed in case data changes while dex menu is already instantiated
+	pass
 
 
 func populate_list():
@@ -13,7 +31,72 @@ func populate_list():
 					create_entry(entry)
 
 
-func create_entry(data: DexEntryData):	
+func create_entry(data: DexEntryData):
 	var dex_entry = entry_scene.instantiate()
 	list.add_child(dex_entry)
+
 	dex_entry.populate_entry(data)
+
+
+func clear_list():
+	if list.get_child_count() == 0:
+		return
+
+	var children = list.get_children()
+	for c in children:
+		list.remove_child(c)
+		c.queue_free()
+
+
+# ---------------------
+# || --- SORTS --- ||
+# ---------------------
+
+func standard_sort(a, b):
+	if a.data_copy.entry_number > b.data_copy.entry_number:
+		return true
+	return false
+
+
+func sort_by_colour(a, b):
+	if a.data_copy.colour > b.data_copy.colour:
+		return true
+	return false
+
+
+func sort_by_rarity(a, b):
+	if a.data_copy.rarity > b.data_copy.rarity:
+		return true
+	return false
+
+
+func sort_by_acquisition(a, b):
+	if a.data_copy.acquired == true and b.data_copy.acquired == false:
+		return true
+
+	if a.data_copy.acquired == b.data_copy.acquired:
+		if a.data_copy.entry_number > b.data_copy.entry_number:
+			return true
+
+	return false
+
+
+# ---------------------
+# || --- SIGNALS --- ||
+# ---------------------
+
+func _on_sort_button_item_selected(index):
+	var children = list.get_children()
+
+	match index:
+		0:
+			children.sort_custom(standard_sort)
+		1:
+			children.sort_custom(sort_by_colour)
+		2:
+			children.sort_custom(sort_by_rarity)
+		3:
+			children.sort_custom(sort_by_acquisition)
+
+	for c in children:
+		list.move_child(c, 0)
