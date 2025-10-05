e095d335d013824b3e2ee015bcae1e552f53b2ae
Daniel Francisco <deny.jo@hotmail.com>
2025-10-05 16:49:35 +0100

    Change mouse position uniform to be updated on mouse motion event instead of on process

--- src/scripts/mouse_circle_uniforms.gd
+++ src/scripts/mouse_circle_uniforms.gd
@@ -8,6 +8,7 @@ func _ready():
 		mat.set("shader_parameter/circle_smooth_width", Global.MOUSE_CIRCLE_SMOOTH_WIDTH)
 
 
-func _process(_delta):
-	for mat in Global.get_all_materials():
-		mat.set("shader_parameter/mouse_position", get_viewport().get_mouse_position())
+func _input(event):
+	if event is InputEventMouseMotion:
+		for mat in Global.get_all_materials():
+			mat.set("shader_parameter/mouse_position", event.position)
