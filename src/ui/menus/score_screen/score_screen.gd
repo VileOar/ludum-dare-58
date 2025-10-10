80c12e442436eb5927ba98e7b4dd607b323cb1cc
Daniel Francisco <deny.jo@hotmail.com>
2025-10-10 18:16:55 +0100

    Fix score screen combo animation

--- src/ui/menus/score_screen/score_screen.gd
+++ src/ui/menus/score_screen/score_screen.gd
@@ -2,6 +2,10 @@ extends Control
 
 
 @export var _spawn_icon_time: float = 0.15
+@export var _time_interval_between_icon_combo_animation: float = 0.1
+@export var _target_scale_icon_combo_animation: Vector2 = Vector2(7.0, 7.0)
+@export var _duration_of_combo_display: float = 1.5
+@export var _score_scale_animation_target: Vector2 = Vector2(1.2, 1.2)
 
 var _all_collected_mooks: Array[MookStats]
 
@@ -43,47 +47,55 @@ func spawn_mook_icons():
 		
 		if _has_scored_a_combo:
 			_has_scored_a_combo = false
-			_display_combo()
-			_play_combo_special_sfx()
 			
-			await get_tree().create_timer(2.0).timeout
+			var animation_player: AnimationPlayer = mook_icon.get_node("TextureRect/AnimationPlayer")
+			if animation_player.is_playing():
+				await animation_player.animation_finished
+
+			_play_combo_special_sfx()
+			await _display_combo()	# we have to wait for display combo to finish animating
 		
 		await get_tree().create_timer(_spawn_icon_time).timeout
 	$Death.play("end" if Global.get_final_score() >= Global.GOOD_SCORE_THRESHOLD else "blink")
 
 
 func _display_combo() -> void:
+	var target_position_container: Control
 	match _combo_length:
+		3:
+			target_position_container = $ThreeComboPositions
 		4:
-			# get last four mooks of the grid
-			for i in range(4, 0, -1):
-				var mook_icon: Control = $GridContainer.get_child($GridContainer.get_child_count() - i)
-				_animate_combo_icon(mook_icon, $FourComboPositions.get_child(i - 1).global_position)
+			target_position_container = $FourComboPositions
 		5:
-			# get last five mooks of the grid
-			for i in range(5, 0, -1):
-				var mook_icon: Control = $GridContainer.get_child($GridContainer.get_child_count() - i)
-				_animate_combo_icon(mook_icon, $FiveComboPositions.get_child(i - 1).global_position)
+			target_position_container = $FiveComboPositions
 		6:
-			# get last six mooks of the grid
-			for i in range(6, 0, -1):
-				var mook_icon: Control = $GridContainer.get_child($GridContainer.get_child_count() - i)
-				_animate_combo_icon(mook_icon, $SixComboPositions.get_child(i - 1).global_position)
+			target_position_container = $SixComboPositions
+	
+	for i in range(_combo_length, 0, -1):
+		var mook_icon: TextureRect = $GridContainer.get_child($GridContainer.get_child_count() - i).get_node("TextureRect")
+		
+		# add a delay between each animation except for last one
+		if (i != 1):
+			_animate_combo_icon(mook_icon, target_position_container.get_child(_combo_length - i).global_position)
+			await get_tree().create_timer(_time_interval_between_icon_combo_animation).timeout
+		else:
+			# on last animation wait for it to finish
+			await _animate_combo_icon(mook_icon, target_position_container.get_child(_combo_length - i).global_position)
 
 
-func _animate_combo_icon(mook_icon, mook_target_position):
-	var animation_player: AnimationPlayer = mook_icon.get_node("TextureRect/AnimationPlayer")
-	if animation_player.is_playing():
-		await animation_player.animation_finished
-	var tween = get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_ELASTIC)
-	tween.tween_property(mook_icon.get_node("TextureRect"), "scale", Vector2(7.0, 7.0), 0.5)
+func _animate_combo_icon(mook_icon: TextureRect, mook_target_position: Vector2):
+	var tween = get_tree().create_tween().set_trans(Tween.TRANS_ELASTIC)
+
+	tween.tween_property(mook_icon, "scale", _target_scale_icon_combo_animation, 0.5)
 	tween.set_parallel(true)
-	tween.tween_property(mook_icon.get_node("TextureRect"), "global_position", mook_target_position, 0.5)
+	tween.tween_property(mook_icon, "global_position", mook_target_position, 0.5)
 	tween.set_parallel(false)
-	tween.tween_property(mook_icon.get_node("TextureRect"), "scale", Vector2(7.0, 7.0), 1.0)
-	tween.tween_property(mook_icon.get_node("TextureRect"), "position", Vector2(0.0, 0.0), 0.5)
+	tween.tween_interval(_duration_of_combo_display)
+	tween.tween_property(mook_icon, "position", Vector2(0.0, 0.0), 0.5)
 	tween.set_parallel(true)
-	tween.tween_property(mook_icon.get_node("TextureRect"), "scale", Vector2(1.0, 1.0), 0.5)
+	tween.tween_property(mook_icon, "scale", Vector2(1.0, 1.0), 0.5)
+
+	await tween.finished
 
 
 func _on_scored_a_combo(combo_score: int, combo_length: int) -> void:
@@ -95,7 +107,7 @@ func _on_scored_a_combo(combo_score: int, combo_length: int) -> void:
 func _update_score_label(new_score: int) -> void:
 	$Score/ScoreValue.text = str(new_score)
 	var tween = get_tree().create_tween()
-	tween.tween_property($Score/ScoreValue, "scale", Vector2(1.2, 1.2), 0.075)
+	tween.tween_property($Score/ScoreValue, "scale", _score_scale_animation_target, 0.075)
 	tween.tween_property($Score/ScoreValue, "scale", Vector2(1.0, 1.0), 0.075)
 
 
@@ -111,6 +123,7 @@ func _on_soul_dex_pressed() -> void:
 	_play_click_sfx()
 	$DexMenu.show()
 
+
 func _on_quit_pressed() -> void:
 	_play_click_sfx()
 	AudioManager.instance.fade_out_music(BACKGROUND_MUSIC)
@@ -118,11 +131,12 @@ func _on_quit_pressed() -> void:
 		get_tree().change_scene_to_file(Global.TITLE_SCENE_FILEPATH)
 	change_scene.call_deferred()
 
+
 #region Audio
 func _play_end_game_audio() -> void:
 	AudioManager.instance.play_audio(BACKGROUND_MUSIC)
-	
-	
+
+
 func _play_special_sfx() -> void:
 	var _final_score = ScoreManager.calculate_total_score()
 	
@@ -134,18 +148,20 @@ func _play_special_sfx() -> void:
 	
 	if _final_score > 600:
 		AudioManager.instance.play_audio_random_pitch("MookScore3", 1.7, 1.9)
-	
+
+
 func _play_combo_special_sfx() -> void:
-	await get_tree().create_timer(0.5).timeout
 	AudioManager.instance.play_audio_random_pitch("ComboScore", 1.5, 1.5)
 	
 
 func _play_click_sfx() -> void:
 	AudioManager.play_click_sfx()
-	
+
+
 func _play_hover_sfx() -> void:
 	AudioManager.play_hover_sfx()
-	
+
+
 func _on_button_mouse_entered():
 	_play_hover_sfx()
 #endregion
